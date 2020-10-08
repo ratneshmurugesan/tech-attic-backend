62cd07e1f855b724eaf897108c5a7e57d81f7cfd
ratneshmurugesan <Ratnesh.one@gmail.com>
2020-10-08 15:00:18 +0530

    Add first route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+// const path = require('path');
 
 const app = express();
 
@@ -8,6 +9,16 @@ const app = express();
 // mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
 
 app.use(bodyParser.json());
+
+// Each node file/script is a process, process has all info about a node app.
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.)
+// }
+app.get('*', (req, res) => {
+    res.json({
+        name: 'node app'
+    });
+})
 
 const PORT = process.env.PORT || 5000;
 
